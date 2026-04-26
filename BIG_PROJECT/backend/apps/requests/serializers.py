88d088584b848e41a71af8129d2283b4dd848c29
truongthihonghoa <153450211+truongthihonghoa@users.noreply.github.com>
2026-04-26 19:45:26 +0700

    Update serializers.py

--- BIG_PROJECT/backend/apps/requests/serializers.py
+++ BIG_PROJECT/backend/apps/requests/serializers.py
@@ -2,6 +2,17 @@ from rest_framework import serializers
 from .models import YeuCau
 
 class YeuCauSerializer(serializers.ModelSerializer):
+    ten_nv = serializers.CharField(source='ma_nv.ho_ten', read_only=True)
+
     class Meta:
         model = YeuCau
-        fields = '__all__'
+        fields = [
+            'ma_yc',
+            'loai_yeu_cau',
+            'ngay_bd',
+            'ngay_kt',
+            'ly_do',
+            'trang_thai',
+            'ma_nv',
+            'ten_nv'
+        ]
