4c34a5431c34a9cb8f3e95075a6df1f08e71f99f
truongthihonghoa <153450211+truongthihonghoa@users.noreply.github.com>
2026-04-26 12:56:15 +0700

    Update serializer.py

--- BIG_PROJECT/backend/apps/accounts/serializer.py
+++ BIG_PROJECT/backend/apps/accounts/serializer.py
@@ -3,14 +3,16 @@ from .models import TaiKhoan
 
 class TaiKhoanSerializer(serializers.ModelSerializer):
     ten_dang_nhap = serializers.CharField(source='user.username', read_only=True)
-    ho_ten = serializers.CharField(source='ma_nv.ho_ten', read_only=True)  # giả sử NhanVien có ho_ten
+    ho_ten = serializers.CharField(source='ma_nv.ho_ten', read_only=True)
     vai_tro = serializers.CharField(read_only=True)
     trang_thai = serializers.CharField(read_only=True)
+
+    is_staff = serializers.BooleanField(source='user.is_staff', read_only=True)
 
     class Meta:
         model = TaiKhoan
         fields = [
-            'id',           # nếu có
+            'id',
             'ten_dang_nhap',
             'ho_ten',
             'vai_tro',
