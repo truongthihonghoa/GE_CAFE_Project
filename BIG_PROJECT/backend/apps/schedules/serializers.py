599cf413ed6d04cc92c907bd1fbfce46a8a713e5
truongthihonghoa <153450211+truongthihonghoa@users.noreply.github.com>
2026-04-26 19:42:26 +0700

    Update serializers.py

--- BIG_PROJECT/backend/apps/schedules/serializers.py
+++ BIG_PROJECT/backend/apps/schedules/serializers.py
@@ -2,6 +2,18 @@ from rest_framework import serializers
 from .models import LichLamViec
 
 class ScheduleSerializer(serializers.ModelSerializer):
+    ten_nv = serializers.CharField(source='ma_nv.ho_ten', read_only=True)
+
     class Meta:
         model = LichLamViec
-        fields = '__all__'
+        fields = [
+            'ma_llv',
+            'ngay_lam',
+            'ca_lam',
+            'trang_thai',
+            'ngay_tao',
+            'ghi_chu',
+            'ma_chi_nhanh',
+            'ma_nv',
+            'ten_nv'
+        ]
